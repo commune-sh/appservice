e82748c949983796c4d230be1079e9850d29c36d
hummingbard <mail@hummingbard.com>
2024-07-15 20:15:59 +0600

    ignore cached messages

--- app/messages.go
+++ app/messages.go
@@ -2,14 +2,10 @@ package app
 
 import (
 	"bytes"
-	"context"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"time"
-
-	"github.com/go-chi/chi/v5"
 )
 
 type cachingResponseWriter struct {
@@ -37,36 +33,41 @@ func (c *App) MessagesProxy() http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		room_id := chi.URLParam(r, "room_id")
-
-		from := r.URL.Query().Get("from")
-		to := r.URL.Query().Get("to")
+		/*
+			room_id := chi.URLParam(r, "room_id")
+			from := r.URL.Query().Get("from")
+			to := r.URL.Query().Get("to")
+		*/
 
 		r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.Config.AppService.AccessToken))
 		w.Header().Del("Access-Control-Allow-Origin")
 
 		// return cached messages if no query params
-		if from == "" && to == "" {
-			cached, err := c.Cache.Messages.Get(context.Background(), room_id).Result()
-			if err == nil && cached != "" {
-				c.Log.Info().Msgf("Found cached messages")
-				w.Header().Set("Content-Type", "application/json")
-				w.Write([]byte(cached))
-				return
+		/*
+			if from == "" && to == "" {
+				cached, err := c.Cache.Messages.Get(context.Background(), room_id).Result()
+				if err == nil && cached != "" {
+					c.Log.Info().Msgf("Found cached messages")
+					w.Header().Set("Content-Type", "application/json")
+					w.Write([]byte(cached))
+					return
+				}
 			}
-		}
+		*/
 
 		crw := &cachingResponseWriter{ResponseWriter: w}
 		proxy.ServeHTTP(crw, r)
 
 		if crw.statusCode == http.StatusOK {
 			// cache messages
-			if from == "" && to == "" {
-				err := c.Cache.Messages.Set(context.Background(), room_id, crw.body.String(), 5*time.Minute).Err()
-				if err != nil {
-					c.Log.Error().Msgf("Couldn't cache messages %v", err)
+			/*
+				if from == "" && to == "" {
+					err := c.Cache.Messages.Set(context.Background(), room_id, crw.body.String(), 5*time.Minute).Err()
+					if err != nil {
+						c.Log.Error().Msgf("Couldn't cache messages %v", err)
+					}
 				}
-			}
+			*/
 		}
 	}
 }
